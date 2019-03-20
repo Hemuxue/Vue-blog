a5a77806131109f548e2a71f8f97cfff08d6b252
hmx <527211775@qq.com>
2019-03-20 22:28:33 +0800

    project icon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    hello world
+    <i class="iconfont icon-github-fill"></i>
     <router-view/>
   </div>
 </template>
@@ -11,13 +13,7 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" >
+  #app
+    // background:#fff;
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'common/stylus/index.styl'
+import 'common/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
